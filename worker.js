9ba0ce7a59e79ae4db1c4776250eb66af7e3af60
Mihai Sucan <mihai.sucan@gmail.com>
2014-02-03 22:26:14 +0200

    'improvements' for test.html

--- worker.js
+++ worker.js
@@ -4,6 +4,14 @@ postMessage("worker init");
 var i = 0;
 onmessage = function(ev)
 {
-  console.log("worker received event", "data", ev.data);
+  console.log("worker received event", ev, "data", ev.data);
   postMessage({i: i++, time: new Date(), });
+  if (ev.data == "time") {
+    console.time("lolz");
+    console.time({i: i, timer: 'foo'});
+  }
+  if (ev.data == "timeEnd") {
+    console.timeEnd("lolz");
+    console.timeEnd({i: i, timer: 'foo'});
+  }
 };
